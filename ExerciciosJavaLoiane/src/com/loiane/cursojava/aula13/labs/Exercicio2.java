96a94a05f1165cadb53963c058db3763bb497038
Matheus21Oliveira <matheus21oliveira@gmail.com>
2022-02-03 23:49:25 -0300

    exercicio loiane trainning

---
+++ ExerciciosJavaLoiane/src/com/loiane/cursojava/aula13/labs/Exercicio2.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int numero;
+		
+		System.out.print("digite um número:_______");
+		numero = teclado.nextInt();
+		
+		System.out.println("O número que você digitou foi " + numero);
+		
+		teclado.close();
+	}
+}
---
+++ ExerciciosJavaLoiane/src/module-info.java
@@ -0,0 +1,3 @@
+module ExerciciosJavaLoiane {
+	
+}
