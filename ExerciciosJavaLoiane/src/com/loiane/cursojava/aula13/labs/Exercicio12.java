3d11b5ef68b4270838a369bc8643d4f9298319fb
Matheus21Oliveira <matheus21oliveira@gmail.com>
2022-02-05 02:30:04 -0300

    exercicios de 8 a 12

---
+++ ExerciciosJavaLoiane/src/com/loiane/cursojava/aula13/labs/Exercicio12.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		double altura, peso;
+		
+		System.out.println("Qual é a sua altura? ");
+		altura = teclado.nextDouble();
+		
+		peso = (72.7*altura)-58;
+		
+		System.out.print("O seu peso ideal será de: " + peso);
+	}
+}
---
+++ ExerciciosJavaLoiane/src/com/loiane/cursojava/aula13/labs/Exercicio13.java
@@ -0,0 +1,5 @@
+package com.loiane.cursojava.aula13.labs;
+
+public class Exercicio13 {
+
+}
